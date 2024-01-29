1c24b84211dc4f3f77bf9954c9c906e166aa7cd7
jbanghw <jbanghw@gmail.com>
2024-01-29 10:34:21 -0500

    Generate User model and create movie detail view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,4 +8,10 @@ class MoviesController < ApplicationController
     res = Net::HTTP::get_response(uri)
     @movies = JSON.parse(res.body)['items'].first(10) if res.is_a?(Net::HTTPSuccess)
   end
+
+  def detail
+    uri = URI('https://tv-api.com/en/API/Title/' + ENV['apiKey'] + '/' + params[:id])
+    res = Net::HTTP::get_response(uri)
+    @movie = JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
+  end
 end
